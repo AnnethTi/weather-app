4e624f365fefa198a6fad25df9d20984fddcd951
Anneth Tinajero <annethtinajero@gmail.com>
2024-06-04 13:37:29 -0600

    Componente principal - Vista Principal

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ function App() {
   const [values, setValues] =  useState('')
   const [icon, setIcon] =  useState('')
 
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=queretaro&lang=es&unit=metric&appid=a8912a10114c8143921521d0e1d34e8d`
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=queretaro&lang=es&units=metric&appid=a8912a10114c8143921521d0e1d34e8d`
 
   const getData = async () => {
     await fetch(URL)
       .then( response => { return response.json() })
       .then( data => {
-        console.log(data)
+        if(data.cod >= 400){
+          setValues(false)
+        }else {
+          setValues(data)
+          console.log(data)
+        }
       } )
       .catch( error => {
         console.log( error )
@@ -21,12 +26,46 @@ function App() {
 
   useEffect(() => {
     getData()
-  }, [])
+  }, [search])
   
+  const handleSearch = (e) => {
+    if( e.key === 'Enter'){
+      setSearch(e.target.value)
+    }
+  } 
+
 
   return (
     <>
+    
+    
+    <div className='container'>
       <h2>React Weather App</h2>
+      <div className='row'>
+        <input 
+        onKeyDown={handleSearch}
+        type="text"
+        autoFocus />
+      </div>
+    </div>
+    
+    <div className='card'>
+      { (values) ? (
+        <div className='card-container'>
+          <h1 className='city-name'> {values.name} </h1>
+          <p className='temp'> {values.main.temp.toFixed(0)}&deg;</p>
+          {/* <img className='icon' src={Icons(icon)} alt="icon-weather" /> */}
+
+          <div className='card-footer'>
+            <p className='temp-max-min'> {values.main.temp_min.toFixed(0)}&deg; | {values.main.temp_max.toFixed(0)}&deg; </p>
+          </div>
+        </div>
+      ) : (
+        <h1></h1>
+      )
+      }
+    </div>
+    
     </>
   )
 }
