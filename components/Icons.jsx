127c9db37881d71a6f057d98b912e805dbd6ce55
Anneth Tinajero <annethtinajero@gmail.com>
2024-06-04 12:50:24 -0600

    Creacion del proyecto

---
+++ components/Icons.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Icons = () => {
+  return (
+    
+    <div>Icons</div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2>React Weather App</h2>
+    </>
+  )
+}
+
+export default App
